d95fe90ed12663c958142619140cecaab1818751
husnuuzulmez <31706166+husnuuzulmez@users.noreply.github.com>
2018-02-10 14:12:22 -0800

    Initial commit

---
+++ app/src/main/java/com/droidrank/tictactoe/MainActivity.java
@@ -0,0 +1,239 @@
+package com.droidrank.tictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity{
+
+
+
+    Button block1, block2, block3, block4, block5, block6, block7, block8, block9, restart;
+    TextView result;
+
+
+    int c[][];
+    int i, j, k = 0;
+    Button b[][];
+     Game game;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        block1 = (Button) findViewById(R.id.bt_block1);
+        block2 = (Button) findViewById(R.id.bt_block2);
+        block3 = (Button) findViewById(R.id.bt_block3);
+        block4 = (Button) findViewById(R.id.bt_block4);
+        block5 = (Button) findViewById(R.id.bt_block5);
+        block6 = (Button) findViewById(R.id.bt_block6);
+        block7 = (Button) findViewById(R.id.bt_block7);
+        block8 = (Button) findViewById(R.id.bt_block8);
+        block9 = (Button) findViewById(R.id.bt_block9);
+        result = (TextView) findViewById(R.id.tv_show_result);
+        restart = (Button) findViewById(R.id.bt_restart_game);
+
+        setBoard();
+        
+
+
+
+        /**
+         * Restarts the game
+         */
+        restart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              setBoard();
+            }
+        });
+
+    }
+
+    private void setBoard() {
+             game = new Game();
+        b = new Button[4][4];
+        c = new int[4][4];
+
+
+
+
+        b[1][3] = (Button) findViewById(R.id.bt_block1);
+        b[1][2] = (Button) findViewById(R.id.bt_block2);
+        b[1][1] = (Button) findViewById(R.id.bt_block3);
+
+
+        b[2][3] = (Button) findViewById(R.id.bt_block4);
+        b[2][2] = (Button) findViewById(R.id.bt_block5);
+        b[2][1] = (Button) findViewById(R.id.bt_block6);
+
+
+        b[3][3] = (Button) findViewById(R.id.bt_block7);
+        b[3][2] = (Button) findViewById(R.id.bt_block8);
+        b[3][1] = (Button) findViewById(R.id.bt_block9);
+
+        for (i = 1; i <= 3; i++) {
+            for (j = 1; j <= 3; j++)
+                c[i][j] = 2;
+    }
+
+       for (i = 1; i <= 3; i++) {
+            for (j = 1; j <= 3; j++) {
+                b[i][j].setOnClickListener(new MyClickListener(i, j));
+                if(!b[i][j].isEnabled()) {
+                    b[i][j].setText(" ");
+                    b[i][j].setEnabled(true);
+                }
+            }
+        }
+
+    }
+
+
+
+    class MyClickListener implements View.OnClickListener {
+        int x;
+        int y;
+
+
+        public MyClickListener(int x, int y) {
+
+
+            this.x = x;
+            this.y = y;
+        }
+
+
+        public void onClick(View view) {
+
+
+            if (b[x][y].isEnabled()) {
+                b[x][y].setEnabled(false);
+                b[x][y].setText("O");
+                c[x][y] = 0;
+                result.setText("");
+                if (!checkBoard()) {
+                    game.takeTurn();
+                }
+            }
+        }
+    }
+
+
+    private class Game {
+        public void takeTurn() {
+            if(c[1][1]==2 &&
+                    ((c[1][2]==0 && c[1][3]==0) ||
+                            (c[2][2]==0 && c[3][3]==0) ||
+                            (c[2][1]==0 && c[3][1]==0))) {
+                markSquare(1,1);
+            } else if (c[1][2]==2 &&
+                    ((c[2][2]==0 && c[3][2]==0) ||
+                            (c[1][1]==0 && c[1][3]==0))) {
+                markSquare(1,2);
+            } else if(c[1][3]==2 &&
+                    ((c[1][1]==0 && c[1][2]==0) ||
+                            (c[3][1]==0 && c[2][2]==0) ||
+                            (c[2][3]==0 && c[3][3]==0))) {
+                markSquare(1,3);
+            } else if(c[2][1]==2 &&
+                    ((c[2][2]==0 && c[2][3]==0) ||
+                            (c[1][1]==0 && c[3][1]==0))){
+                markSquare(2,1);
+            } else if(c[2][2]==2 &&
+                    ((c[1][1]==0 && c[3][3]==0) ||
+                            (c[1][2]==0 && c[3][2]==0) ||
+                            (c[3][1]==0 && c[1][3]==0) ||
+                            (c[2][1]==0 && c[2][3]==0))) {
+                markSquare(2,2);
+            } else if(c[2][3]==2 &&
+                    ((c[2][1]==0 && c[2][2]==0) ||
+                            (c[1][3]==0 && c[3][3]==0))) {
+                markSquare(2,3);
+            } else if(c[3][1]==2 &&
+                    ((c[1][1]==0 && c[2][1]==0) ||
+                            (c[3][2]==0 && c[3][3]==0) ||
+                            (c[2][2]==0 && c[1][3]==0))){
+                markSquare(3,1);
+            } else if(c[3][2]==2 &&
+                    ((c[1][2]==0 && c[2][2]==0) ||
+                            (c[3][1]==0 && c[3][3]==0))) {
+                markSquare(3,2);
+            }else if( c[3][3]==2 &&
+                    ((c[1][1]==0 && c[2][2]==0) ||
+                            (c[1][3]==0 && c[2][3]==0) ||
+                            (c[3][1]==0 && c[3][2]==0))) {
+                markSquare(3,3);
+            } else {
+                Random rand = new Random();
+
+                int a = rand.nextInt(4);
+                int b = rand.nextInt(4);
+                while(a==0 || b==0 || c[a][b]!=2) {
+                    a = rand.nextInt(4);
+                    b = rand.nextInt(4);
+                }
+                markSquare(a,b);
+            }
+        }
+
+    }
+
+
+    private void markSquare(int x, int y) {
+        b[x][y].setEnabled(false);
+        b[x][y].setText("X");
+        c[x][y] = 1;
+        checkBoard();
+    }
+
+
+    // check the board to see if someone has won
+    private boolean checkBoard() {
+        boolean gameOver = false;
+        if ((c[1][1] == 0 && c[2][2] == 0 && c[3][3] == 0)
+                || (c[1][3] == 0 && c[2][2] == 0 && c[3][1] == 0)
+                || (c[1][2] == 0 && c[2][2] == 0 && c[3][2] == 0)
+                || (c[1][3] == 0 && c[2][3] == 0 && c[3][3] == 0)
+                || (c[1][1] == 0 && c[1][2] == 0 && c[1][3] == 0)
+                || (c[2][1] == 0 && c[2][2] == 0 && c[2][3] == 0)
+                || (c[3][1] == 0 && c[3][2] == 0 && c[3][3] == 0)
+                || (c[1][1] == 0 && c[2][1] == 0 && c[3][1] == 0)) {
+            result.setText("Game over. You win!");
+            gameOver = true;
+        } else if ((c[1][1] == 1 && c[2][2] == 1 && c[3][3] == 1)
+                || (c[1][3] == 1 && c[2][2] == 1 && c[3][1] == 1)
+                || (c[1][2] == 1 && c[2][2] == 1 && c[3][2] == 1)
+                || (c[1][3] == 1 && c[2][3] == 1 && c[3][3] == 1)
+                || (c[1][1] == 1 && c[1][2] == 1 && c[1][3] == 1)
+                || (c[2][1] == 1 && c[2][2] == 1 && c[2][3] == 1)
+                || (c[3][1] == 1 && c[3][2] == 1 && c[3][3] == 1)
+                || (c[1][1] == 1 && c[2][1] == 1 && c[3][1] == 1)) {
+            result.setText("Game over. You lost!");
+            gameOver = true;
+        } else {
+            boolean empty = false;
+            for(i=1; i<=3; i++) {
+                for(j=1; j<=3; j++) {
+                    if(c[i][j]==2) {
+                        empty = true;
+                        break;
+                    }
+                }
+            }
+            if(!empty) {
+                gameOver = true;
+                result.setText("Game over. It's a draw!");
+            }
+        }
+        return gameOver;
+    }
+
+}
